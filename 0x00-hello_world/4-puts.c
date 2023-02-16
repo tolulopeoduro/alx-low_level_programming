2ba7a3fdd9f19003a3112d6305db7d0dc59be682
Tolu <tolulopeoduro345@gmail.com>
2023-02-16 13:28:53 +0100

    1-compiler

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 int main(void)
 
-/*
-* main - Entry
-*
-* return (0) - Success
-*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
