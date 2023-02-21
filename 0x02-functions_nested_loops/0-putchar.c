ef3153644fcc1236709049d33cf7da02ffaf0944
Tolu <tolulopeoduro345@gmail.com>
2023-02-21 18:53:40 +0100

    m

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,20 @@
-#include <unistd.h>
-
+include "main.h";
 
 /**
- * _putchar - writes
- * 
- * Return: On Success -1
- * On error, -1
+ * int main prints
+ * Return : 0
 */
 
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+	return (0);
 }
